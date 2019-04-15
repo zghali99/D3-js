1593ef477e8a2dd7185607ee13afde29e7dd24d3
zghali99 <44450093+zghali99@users.noreply.github.com>
2019-04-15 18:25:32 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+// @TODO: YOUR CODE HERE!
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 60,
+  left: 50
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Step 2: Create an SVG wrapper,
+// append an SVG group that will hold our chart,
+// and shift the latter by left and top margins.
+// =================================
+var svg = d3
+  .select("body")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+  
+// Step 3:
+// Import data from the donuts.csv file
+// =================================
+d3.csv("data.csv", function(error, stateData) {
+    if (error) throw error;
+  
+    // Format the data
+    stateData.forEach(function(data) {
+      data.poverty = +data.poverty;
+      data.age = +data.age;
+      data.income = +data.income;
+      data.healthcare = +data.healthcare;
+      data.obesity = +data.obesity;
+      data.smokes = +data.smokes;
+    });
